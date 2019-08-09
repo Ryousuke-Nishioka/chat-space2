8bba2c117ed008c546bac87f74bf4a2f09ea89c1
Ryousuke-Nishioka <ry0sk6336.oki@gmail.com>
2019-08-09 18:47:06 +0900

    chat-space2##ok

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,12 +2,10 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validates :group_id, :user_id, presence: true, numericality: {only_integer: true}
+  validates :content, presence: true, unless: :image?
 
-  private
+  mount_uploader :image, ImageUploader
 
-  def body_or_image
-    body.presence or image.presence
-  end
+ 
 
 end
